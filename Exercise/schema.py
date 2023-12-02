7e77ce79802199b90b9ddb6ff7338e9678519529
Dane knight <knight1seven@gmail.com>
2023-12-01 21:31:25 -0500

    practice schema.py

---
+++ Exercise/schema.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+def schema():
+    with sqlite3.connect("examlpe.db") as conn:
+        cursor = conn.cursor()
+        sql = """CREATE TABLE students(
+            pk INTEGER PRIMARY KEY AUTOINCREMENT,
+            name VARCHAR
+        )"""
+        cursor.execute(sql)
+
+
+
+if __name__ == "__main__":
+    schema()
+
+
